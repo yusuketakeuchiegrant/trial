2179a2b18a2603f30942330885aa673da6f5420b
Apache <apache@56d13ad03e71.(none)>
2020-04-08 07:49:47 +0000

    Add the git plugin.

--- git_auto_merger.php
+++ git_auto_merger.php
@@ -3,4 +3,11 @@ require_once('Git.php');
 
 $repo = Git::open('.');
 var_dump($repo);
+$result = $repo->test_git();
+var_dump($result);
+$result = $repo->commit('Add the git plugin.');
+var_dump($result);
+$branch_list = $repo->list_branches();
+var_dump($branch_list);
+$repo->checkout('master');
 ?>
