90801113a978c6468bea4d72c0250855028af609
竹内雄亮 <yusuke.takeuchi@e-grant.net>
2020-04-08 17:45:50 +0900

    to create other branch.

--- git_auto_merger.php
+++ git_auto_merger.php
@@ -1,15 +1,34 @@
 <?php
 require_once('Git.php');
 
-$repo = Git::open('.');
-var_dump($repo);
-$result = $repo->test_git();
-var_dump($result);
-// $result = $repo->commit('Add the git plugin.');
-// var_dump($result);
-$branch_list = $repo->list_branches();
-var_dump($branch_list);
-$repo->checkout('branch');
-$current = $repo->active_branch();
-var_dump($current);
+/**
+ * 
+ */
+class GitAutoMerger
+{
+    public function merge_to_sale_dev($blanch='')
+    {
+        $path_to_kozuchi_repo = '.';
+        $repo = Git::open($path_to_kozuchi_repo);
+
+        $active_blanch = $repo->active_branch();
+        var_dump($active_blanch);
+        $sale_dev = '_sale_dev';
+        if ($active_blanch !== $sale_dev) {
+            // If this fails, throws exception.
+            $repo->checkout($sale_dev);
+        }
+
+        $remote_branches = $repo->list_remote_branches();
+        if (in_array($blanch, $remote_branches)) {
+
+        }
+
+        $blanch = '';
+    }    
+}
+
+
+$gam = new GitAutoMerger();
+$gam->merge_to_sale_dev();
 ?>
