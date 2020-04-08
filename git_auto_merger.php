7dccb6d84a4958d272a0f458357074e8599904a2
竹内雄亮 <yusuke.takeuchi@e-grant.net>
2020-04-08 17:02:21 +0900

    no message

---
+++ git_auto_merger.php
@@ -0,0 +1,15 @@
+<?php
+require_once('Git.php');
+
+$repo = Git::open('.');
+var_dump($repo);
+$result = $repo->test_git();
+var_dump($result);
+// $result = $repo->commit('Add the git plugin.');
+// var_dump($result);
+$branch_list = $repo->list_branches();
+var_dump($branch_list);
+$repo->checkout('branch');
+$current = $repo->active_branch();
+var_dump($current);
+?>
